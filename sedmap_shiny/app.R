c6a5f9f56f7022537863426c11ff6a109cd8e9b7
annakrystalli <annakrystalli@googlemail.com>
2018-09-27 14:18:13 +0100

    stack rst, include layer navpanel

--- sedmap_shiny/app.R
+++ sedmap_shiny/app.R
@@ -18,16 +18,21 @@ library(htmltools)
 sed_path <- here::here("data-raw", "nc", "sediment_properties.nc")
 dis_path <- here::here("data-raw", "nc", "monthly_disturbance.nc")
 
-data <- "sed"
-
-rst <- switch(data,
-              "dis" = {ncdf_dimstack(dis_path, dimension = "Time") %>% 
-                      setNames(month.name[names(.) %>% 
-                                              stringr::str_replace("month_", "") %>% 
-                                              as.numeric()])},
-              "sed" = {ncdf_stack(sed_path)})
 
 
+rst <- list(
+    "sed" = {ncdf_stack(sed_path)},
+    "dis" = {ncdf_dimstack(dis_path, dimension = "Time") %>% 
+            setNames(month.name[names(.) %>% 
+                                    stringr::str_replace("month_", "") %>% 
+                                    as.numeric()])})
+
+varnames <- list(sed = names(rst[["sed"]]),
+                 dis = names(rst[["dis"]]))
+
+rst <- raster::stack(rst)
+
+names(rst)
 # Define UI for application that draws a histogram
 ui <- fluidPage(theme = shinythemes::shinytheme("superhero"),
                 
@@ -38,33 +43,41 @@ ui <- fluidPage(theme = shinythemes::shinytheme("superhero"),
                 # Leaflet plot 
                 leafletOutput("leaflet", width = "100%",  height="1000px"),
                 
+                # layer selection panel
                 absolutePanel(
-                    top = 70, left = 20, width = 200,
+                    top = 70, left = 20, width = 270,
                     draggable = TRUE,
                     wellPanel(
-                        h3("Layers"), 
-                        hr(),
-                        # selectors
-                        radioButtons("varname", label = h5("Select layer"),
-                                     choices = names(rst), 
-                                     selected = names(rst)[1]))),
+                        h3("Layers"),
+                        tabsetPanel(id = "data", type = "tabs", selected = "sed",
+                                    tabPanel("Sediment", value = "sed",
+                                             hr(),
+                                             radioButtons("varname_sed", label = h4("Select layer"),
+                                                          choices =  varnames[["sed"]], 
+                                                          selected =  varnames[["sed"]][1])),
+                                    tabPanel("Disturbance",  value = "dis",
+                                             hr(),
+                                             radioButtons("varname_dis", label = h5("Select layer"),
+                                                          choices = varnames[["dis"]]), 
+                                             selected = varnames[["dis"]][1])))),
+                # Map tools
                 absolutePanel(
                     bottom = 20, right = 20, width = 200,
                     draggable = TRUE,
                     wellPanel(opacity = 0.8,
-                        h3("Map tools"),
-                        hr(),
-                        sliderInput("opacity",h5("Layer opacity"),
-                                    min = 0,
-                                    max = 1,
-                                    value = 0.8),
-                        selectInput("option", label = h5("Palette"), 
-                                    choices = list("magma" = "A", "inferno" = "B",
-                                                   "plasma" = "C", "viridis" = "D"), 
-                                    selected = "A"),
-                        selectInput("basemap", label = h5("Basemap"), 
-                                    choices = leaflet::providers, 
-                                    selected = "Esri.OceanBasemap")
+                              h3("Map tools"),
+                              hr(),
+                              sliderInput("opacity",h5("Layer opacity"),
+                                          min = 0,
+                                          max = 1,
+                                          value = 0.8),
+                              selectInput("option", label = h5("Palette"), 
+                                          choices = list("magma" = "A", "inferno" = "B",
+                                                         "plasma" = "C", "viridis" = "D"), 
+                                          selected = "A"),
+                              selectInput("basemap", label = h5("Basemap"), 
+                                          choices = leaflet::providers, 
+                                          selected = "Esri.OceanBasemap")
                     )
                 )
 )
@@ -73,10 +86,13 @@ ui <- fluidPage(theme = shinythemes::shinytheme("superhero"),
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-    
+    #v <- reactiveValues(varname = input$varname_sed)
+    get_varname <- reactive({switch(input$data,
+                                    "sed" = input$varname_sed,
+                                    "dis" = input$varname_dis)})
     output$leaflet <- renderLeaflet({
         
-        lflt_plot(rst, varname = input$varname, label = "Disturbance", 
+        lflt_plot(rst, varname = get_varname(), label = "Disturbance", 
                   opacity = input$opacity, option = input$option,
                   basemap = input$basemap) 
         
